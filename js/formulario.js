7c58b0101e79050eb2f057b8ac336e3829b4c012
pedro <paarruvito@hotmail.com>
2022-03-22 23:46:11 -0300

    subiendo js

---
+++ js/formulario.js
@@ -0,0 +1,42 @@
+function validarDatos(){
+  window.event.preventDefault();
+ 
+  if(document.formulario.nombre.value == ""){
+    alert("Campo Nombre es obligatorio")
+    document.formulario.nombre.focus();
+  }
+  else if (document.formulario.email.value == ""){
+    alert("Campo Email es obligatorio")
+    document.formulario.email.focus();
+    }else if (document.formulario.email.value.indexOf("@")==-1 && document.formulario.email.value.indexOf(".") == -1){
+      alert("El Email es invalido")
+      document.formulario.email.focus();
+
+  }else if  (document.formulario.asunto.value == ""){
+    alert("Campo Asunto es obligatorio")
+    document.formulario.asunto.focus();
+  }else if  (document.formulario.mensaje.value == ""){
+  alert("Campo Mensaje es obligatorio")
+  document.formulario.mensaje.focus();
+  }else{
+    document.formulario.reset()
+    }
+}
+
+document.querySelector("form").addEventListener("submit", validarDatos);
+
+
+let input = document.querySelector(".input-padron");
+let button = document.querySelector(".boton-enviar");
+button.disabled = true;
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+  if (document.querySelector(".input-padron").value === "") {
+    button.disabled = true; 
+  } else {
+    button.disabled = false;
+  }
+}
+
+
